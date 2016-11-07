06932dad639c30324a34bd5f244762e0ce7221a4
Steffen Remus <steremus@gmail.com>
2016-11-07 15:31:55 +0100

    copy server conf everytime

--- Docker/build_and_run_docker.sh
+++ Docker/build_and_run_docker.sh
@@ -4,4 +4,6 @@ docker build . -t moviecharacterinsight:demo
 
 docker run -d -p ${1:-8181}:80 --name moviecharacterinsights moviecharacterinsight:demo
 
+docker cp server.user.conf moviecharacterinsights:/Server/
+
 docker logs -f moviecharacterinsights
